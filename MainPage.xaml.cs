f0d1cb3b8be7f330712831a7dd3c64cba338c122
Amiraj Nigam <amiraj.nigam@intel.com>
2023-04-24 13:57:24 -0700

    UWP app C# Code for toast notficiation

---
+++ MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.Toolkit.Uwp.Notifications;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Toast_notification_UWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage() //constructor of class MainPage
+        {
+            this.InitializeComponent();
+
+            // Requires Microsoft.Toolkit.Uwp.Notifications NuGet package version 7.0 or greater
+            new ToastContentBuilder()
+                .AddArgument("action", "viewConversation")
+                .AddArgument("conversationId", 9813)
+                .AddText("Third Monitor not supported")
+                .AddText("This PC supports only two Thunderbolt monitors")
+                .AddAppLogoOverride(new Uri("ms-appx:///Assets/monitor.jpg"), ToastGenericAppLogoCrop.Circle)
+                .Show();
+        }
+    }
+}
